0219c0dc892a3f99b64dd1fa33d0b39c9f45df8b
sethiyanisal <sethiya1098@gmail.com>
2022-09-09 12:17:26 +0530

    fixed the errors and added the navigation through buttons

--- mob_app/lib/main.dart
+++ mob_app/lib/main.dart
@@ -12,68 +12,97 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String buttonName = 'Login';
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String buttonName1 = 'Login';
+  String buttonName2 = 'Next Page';
   int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ceylonese'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonName1 = 'Logged In';
+                        });
+                      },
+                      child: Text(buttonName1),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return const SecondPage();
+                            },
+                          ),
+                        );
+                      },
+                      child: Text(buttonName2),
+                    ),
+                  ],
+                ),
+              )
+            : Image.asset('images/beach.jpg'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          )
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ceylonese'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  color: Colors.red,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.orange,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Logged In';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Logged In';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                    ],
-                  ),
-                )
-              : Image.asset('images/beach.jpg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: 'Settings',
-              icon: Icon(Icons.settings),
-            )
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
