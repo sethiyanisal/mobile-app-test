3d44447447650eb7b13cd85e808eac2baf19e1de
sethiyanisal <sethiya1098@gmail.com>
2022-09-09 11:49:36 +0530

    Reverted some commits

--- mob_app/test/widget_test.dart
+++ mob_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mob_app/main2.dart';
+import 'package:mob_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
